c8c0552a9cb103226ab760c62669bf9dc2990c1b
Anderson <anderson.amachinery@gmail.com>
2021-09-26 14:36:02 -0300

    Adicionados eventos de mouseOver e mouseMove ao Slide

---
+++ js/slide.js
@@ -0,0 +1,38 @@
+export default class Slide {
+  constructor(slide, wrapper) {
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+  }
+
+  onStart(event) {
+    event.preventDefault();
+    console.log('mousedown');
+    this.wrapper.addEventListener('mousemove', this.onMove);
+  }
+
+  onMove(event) {
+    console.log('moveu');
+  }
+
+  onEnd() {
+    console.log('acabou');
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+  }
+
+  addSlideEvents() {
+    this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('mouseup', this.onEnd);
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+  }
+
+  init() {
+    this.bindEvents();
+    this.addSlideEvents();
+    return this;
+  }
+}
